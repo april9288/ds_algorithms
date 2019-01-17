ba741235bcf25107387d07a1ceca76d1038b6c7b
Jongho Kim <april9288@gmail.com>
2019-01-17 13:27:03 -0800

    done with stack py

--- stack.py
+++ stack.py
@@ -15,5 +15,31 @@ class Stack:
 	def isEmpty(self):
 		return bool(self.length is 0)
 
+	def push(self,value):
+		newNode = Node(value)
+		if (self.length == 0):
+			self.top = newNode
+			self.bottom = newNode
+		else:
+			originalTop = self.top
+			self.top = newNode
+			newNode.next = originalTop
+		self.length += 1
+		return self
+
+	def pop(self):
+		if (self.length <= 0):
+			return None
+		elif (self.length == 1):
+			popped = self.top.value
+			self.top = None
+			self.bottom = None
+		else:
+			popped = self.top.value
+			self.top = self.top.next
+		self.length -= 1
+		return popped
+
+
+
 myStack = Stack()
-print(myStack.isEmpty())
