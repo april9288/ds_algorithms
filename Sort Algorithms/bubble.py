a3a4845f9ff1a662835f3564d9edf3ba5857f2b5
Jongho Kim <april9288@gmail.com>
2019-01-19 11:02:12 -0800

    updating bubble py

--- Sort Algorithms/bubble.py
+++ Sort Algorithms/bubble.py
@@ -1,11 +1,8 @@
 def bubbleSort(array):
-	arr_length = len(array)
-	for i in range(arr_length, 1, -1):
-		for j in range(0, i-1):
-			if (array[j] > array[j+1]) :
-				temp = array[j]
-				array[j] = array[j+1]
-				array[j+1] = temp
+
+	
+
+
 	return array
 
 
