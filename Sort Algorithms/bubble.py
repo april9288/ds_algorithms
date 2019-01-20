a8d19a8d6b93707d93eb1b7ffb2827f35bfff92c
Jongho Kim <april9288@gmail.com>
2019-01-19 16:17:18 -0800

    updating bubble py

--- Sort Algorithms/bubble.py
+++ Sort Algorithms/bubble.py
@@ -1,18 +1,12 @@
 def bubbleSort(array):
-
 	arrayLength = len(array)
 	for i in range(arrayLength, 1, -1):
-		print(f'upper loop i is {i}')
 		for j in range(0, i-1):
-			print(f'i is {i} and j is {j}')
 			if (array[j] > array[j+1]):
-				temp= array[j]
+				temp = array[j]
 				array[j] = array[j+1]
 				array[j+1] = temp
-
-
 	return array
-
 
 example1 = [5,4,8,2,1,5,3,9]
 example2 = [9,3,7,4,6]
