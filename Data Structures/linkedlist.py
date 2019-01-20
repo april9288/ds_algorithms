0b406a6efabd86fcb30be0b4cc6185960fabea92
Jongho Kim <april9288@gmail.com>
2019-01-19 22:53:22 -0800

    updating linked list py

--- Data Structures/linkedlist.py
+++ Data Structures/linkedlist.py
@@ -4,74 +4,82 @@ class Node:
 		self.next = None
 
 class LinkedList:
-	def __init__(self,value):
-		newNode = Node(value)
-		self.head = newNode
-		self.tail = self.head
-		self.length = 1
+	def __init__(self):
+		self.head = None
+		self.tail = None
+		self.length = 0
 
 	def append(self,value):
 		newNode = Node(value)
-		self.tail.next = newNode
-		self.tail = newNode
+		if (self.length == 0):
+			self.head = newNode
+			self.tail = self.head
+		else:
+			self.tail.next = newNode
+			self.tail = newNode
 		self.length += 1
 		return self
 
 	def prepend(self,value):
 		newNode = Node(value)
-		newNode.next = self.head
-		self.head = newNode
+		if (self.length == 0):
+			self.head = newNode
+			self.tail = self.head
+		else:
+			newNode.next = self.head
+			self.head = newNode
 		self.length += 1
 		return self
 
 	def printList(self):
-		array = []
-		counter = 0
 		currentNode = self.head
-		while (counter != self.length):
+		array = []
+		while(currentNode):
 			array.append(currentNode.value)
 			currentNode = currentNode.next
-			counter += 1
-		return array
+		return array	
 
 	def insert(self, index, value):
-		if index == 0:
+		if (index < 0 or index > self.length):
+			return "Wrong index"
+
+		if (index == 0):
 			self.prepend(value)
-		elif index >= self.length:
+
+		elif (index == self.length):
 			self.append(value)
+
 		else:
 			newNode = Node(value)
-			leader = self.traverse(index-1)
+			leader = self.traverse(index - 1)
 			newNode.next = leader.next
-			leader.next =newNode
+			leader.next = newNode
 			self.length += 1
 			return self
 
 	def traverse(self, index):
-		leader = self.head
+		currentNode = self.head
 		counter = 0
 		while(counter != index):
-			leader = leader.next
-			counter +=1
-		return leader
+			currentNode = currentNode.next
+			counter += 1
+		return currentNode
 
 	def remove(self, index):
-		if index == 0:
+		if (self.length == 0 or index < 0 or index >= self.length):
+			return "Wrong index"
+
+		if (index == 0):
 			self.head = self.head.next
-			self.length -= 1
-		elif index == self.length-1:
-			leader = self.traverse(index-1)
+		elif (index == self.length - 1):
+			leader = self.traverse(index - 1)
 			leader.next = None
 			self.tail = leader
-			self.length -= 1
-		elif index >= self.length:
-			return print("Wrong Index. Try again")
 		else:
-			leader = self.traverse(index-1)
+			leader = self.traverse(index - 1)
 			willBeRemoved = leader.next
-			leader.next =willBeRemoved.next
-			self.length -=1
+			leader.next = willBeRemoved.next
+		self.length -= 1
+		return self		
 
-		
-
-myLL = LinkedList(5)
+myLL = LinkedList()
