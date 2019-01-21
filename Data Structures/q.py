705a7ff822bb00ee951e49c53cc327acffef0d06
Jongho Kim <april9288@gmail.com>
2019-01-20 23:26:23 -0800

    updating queue py

--- Data Structures/q.py
+++ Data Structures/q.py
@@ -1,50 +1,43 @@
+class Node:
+	def __init__(self, value):
+		self.value = value
+		self.next = None
 
+class Queue:
+	def __init__(self):
+		self.first = None
+		self.last = None
+		self.length = 0
 
+	def peek(self):
+		return self.first
 
+	def isEmpty(self):
+		return bool(self.length is 0)
 
-# class Node:
-# 	def __init__(self, value):
-# 		self.value = value
-# 		self.next = None
+	def push(self,value):
+		newNode = Node(value)
+		if (self.length == 0):
+			self.first = newNode
+			self.last = self.first
+		else:
+			self.last.next = newNode
+			self.last = newNode
+		self.length += 1
+		return self
 
-# class Queue:
-# 	def __init__(self):
-# 		self.first = None
-# 		self.last = None
-# 		self.length = 0
+	def pop(self):
+		if (self.length == 0):
+			return "Nothing to return"
 
-# 	def peek(self):
-# 		return self.first
+		popped = self.first
+		if (self.length == 1):
+			self.first = None
+			self.last = None
+		else:
+			self.first = self.first.next
 
-# 	def isEmpty(self):
-# 		return bool(self.length is 0)
+		self.length -= 1
+		return popped
 
-# 	def nq(self,value):
-# 		newNode = Node(value)
-# 		if (self.length == 0):
-# 			self.first = newNode
-# 			self.last = newNode
-# 		else:
-# 			self.last.next = newNode
-# 			self.last = newNode
-# 		self.length += 1
-# 		return self
-
-# 	def dq(self):
-# 		if (self.length <= 0):
-# 			return None
-# 		elif (self.length == 1):
-# 			dq = self.first
-# 			self.first = None
-# 			self.last = None
-# 		else:
-# 			dq = self.first
-# 			self.first = self.first.next
-
-# 		self.length -= 1
-# 		return dq
-
-
-
-# myQueue = Queue()
-
+myQ = Queue()
