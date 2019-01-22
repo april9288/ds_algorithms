5845607dca48321b00f4e24f0ab592e91832eada
Jongho Kim <april9288@gmail.com>
2019-01-21 21:19:28 -0800

    updating array js

--- Data Structures/array.js
+++ Data Structures/array.js
@@ -7,24 +7,53 @@ class myArray{
 		return this.data[index]
 	}
 	push(item){
-
+		this.data[this.length] = item
+		this.length++
+		return this
 	}
 	pop(){
-
+		if (this.length !== 0) {
+			let popped = this.data[this.length-1]
+			delete this.data[this.length-1]
+			this.length--
+			return popped
+		} else {
+			return "Nothing to pop"
+		}
 	}
 	shift(){
+		if (this.length !== 0) {
+			let popped = this.data[0]
+			let i = 0
+			while(i < this.length) {
+				this.data[i] = this.data[i+1]
+				i++
+			}
+			delete this.data[this.length-1]
+			this.length--
+			return popped
+		} else {
+			return "Nothing to shift"
+		}
+	}
+	unshift(item){
+		
+	}
+	insert(index, item){
 
 	}
-	unshift(){
-
-	}
-	insert(){
-
-	}
-	remove(){
+	remove(index){
 
 	}
 	printArray(){
-		
+		return Object.values(this.data)
 	}
 }
+
+let mymymymyArray = new myArray()
+mymymymyArray.push(0)
+mymymymyArray.push(10)
+mymymymyArray.push(20)
+console.log("shifted value : " , mymymymyArray.shift())
+console.log("current array : " , mymymymyArray.printArray())
+console.log(mymymymyArray)
