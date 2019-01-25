fa6260ae762d2678833fdb0ac00298d0a89924e0
Jongho Kim <april9288@gmail.com>
2019-01-24 23:13:46 -0800

    updating singly linked list reverse function in py

--- Data Structures/singlyLinkedList.js
+++ Data Structures/singlyLinkedList.js
@@ -1,14 +1,18 @@
 class Node {
-	constructor(value){
+	constructor(value) {
 		this.value = value
 		this.next = null
 	}
 }
-class SinglyLL {
-	constructor(){
+
+class LinkedList {
+	constructor() {
 		this.head = null
 		this.tail = null
 		this.length = 0
+	}
+	isEmpty() {
+		return Boolean(!this.length)
 	}
 	append(value){
 		let newNode = new Node(value)
@@ -24,7 +28,7 @@ class SinglyLL {
 	}
 	prepend(value){
 		let newNode = new Node(value)
-		if (this.length === 0) {
+		if (this.lenght === 0) {
 			this.head = newNode
 			this.tail = this.head
 		} else {
@@ -34,80 +38,66 @@ class SinglyLL {
 		this.length++
 		return this
 	}
-	insert(index, value){
-		if (index < 0 || index > this.length) {
-			return "Wrong input"
+	insert(index, value) {
+		if (index < 0 || index > this.length || this.length === 0) {
+			return "Wrong index"
 		}
-
-		if (this.length === 0 || index === this.length) {
-			return this.append(value)
-		}
-
 		if (index === 0) {
 			return this.prepend(value)
 		}
-
+		if (index === this.length) {
+			return this.append(value)
+		}
 		let newNode = new Node(value)
-		let leader = this.traverse(index - 1)
-		newNode.next = leader.next
-		leader.next = newNode
+		let curr = this.traverse(index-1)
+		newNode.next = curr.next
+		curr.next = newNode
 		this.length++
 		return this
 	}
-	remove(index){
-		if (this.length === 0 || index < 0 || index >= this.length) {
-			return "Nothing to remove"
-		}
-		if (this.length === 1) {
-			let popped = this.head
-			this.head = null
-			this.tail = this.head
-			this.length--
-			return popped
-		}
-		if (index === 0) {
-			let popped = this.head
-			this.head = this.head.next
-			this.length--
-			return popped
-		}
-		if (index === this.length-1) {
-			let popped = this.tail
-			let leader = this.traverse(index-1)
-			leader.next = null
-			this.tail = leader
-			this.length--
-			return popped
-		} else {
-			let leader = this.traverse(index-1)
-			let willBeRemoved = leader.next
-			leader.next = willBeRemoved.next
-			this.length--
-			return willBeRemoved
-		}
-	}
-	traverse(index){
+	traverse(index) {
 		let curr = this.head
 		let counter = 0
-		while (counter < index) {
+		while (counter < index){
 			curr = curr.next
 			counter++
 		}
 		return curr
 	}
-	set(index, value){
-		if (this.length === 0 || index < 0 || index >= this.length){
-			return "Nothing to Set"
+	set(index, value) {
+		if (index < 0 || index >= this.length){
+			return "Nothing to set"
 		}
-
 		let curr = this.traverse(index)
 		curr.value = value
-		return curr
+		return this
 	}
-	isEmpty() {
-		return Boolean(!this.length)
+	remove(index) {
+		if (this.length === 0 || index < 0 || index >= this.length) {
+			return "Nothing to remove"
+		}
+		let popped
+		if (this.length === 1 && index === 0) {
+			popped = this.head
+			this.head = null
+			this.tail = this.head
+		} else if (index === 0){
+			popped = this.head
+			this.head = this.head.next
+		} else if (index === this.length-1) {
+			popped = this.tail
+			let leader = this.traverse(index-1)
+			leader.next = null
+			this.tail = leader
+		} else {
+			let leader = this.traverse(index-1)
+			popped = leader.next
+			leader.next = popped.next
+		}
+		this.length--
+		return popped
 	}
-	printList() {
+	print() {
 		let list = []
 		let curr = this.head
 		while(curr) {
@@ -117,28 +107,30 @@ class SinglyLL {
 		return list
 	}
 	reverse(){
-		if (this.length < 2) {
-			return "Nothing to return"
-		}
 		let first = this.head
 		let second = first.next
 		let third = second.next
 
 		first.next = null
 		this.tail = first
-
-		
-		while(third) {
+		while (third) {
 			second.next = first
-
 			first = second
 			second = third
 			third = second.next
 		}
 		second.next = first
 		this.head = second
+		return this
 	}
-
 }
 
-let mylist = new SinglyLL()
+
+let myLL = new LinkedList()
+// myLL.append(10)
+// myLL.prepend(5)
+// myLL.append(15)
+// myLL.append(20)
+
+// console.log(myLL.print())
+// console.log(myLL)
--- Data Structures/singlyLinkedList.py
+++ Data Structures/singlyLinkedList.py
@@ -1,5 +1,5 @@
 class Node:
-	def __init__(self,value):
+	def __init__(self, value):
 		self.value = value
 		self.next = None
 
@@ -13,73 +13,123 @@ class LinkedList:
 		newNode = Node(value)
 		if (self.length == 0):
 			self.head = newNode
-			self.tail = self.head
+			self.tail = newNode
 		else:
 			self.tail.next = newNode
 			self.tail = newNode
-		self.length += 1
+
+		self.length+=1
 		return self
 
 	def prepend(self,value):
 		newNode = Node(value)
 		if (self.length == 0):
 			self.head = newNode
-			self.tail = self.head
+			self.tail = newNode
 		else:
 			newNode.next = self.head
 			self.head = newNode
-		self.length += 1
+
+		self.length+=1
 		return self
 
-	def printList(self):
-		currentNode = self.head
-		array = []
-		while(currentNode):
-			array.append(currentNode.value)
-			currentNode = currentNode.next
-		return array	
-
-	def insert(self, index, value):
+	def insert(self,index,value):
 		if (index < 0 or index > self.length):
-			return "Wrong index"
+			return "Nothing to insert"
 
 		if (index == 0):
-			self.prepend(value)
+			return self.prepend(value)
 
-		elif (index == self.length):
-			self.append(value)
+		if (index == self.length):
+			return self.append(value)
 
-		else:
-			newNode = Node(value)
-			leader = self.traverse(index - 1)
-			newNode.next = leader.next
-			leader.next = newNode
-			self.length += 1
-			return self
+		newNode = Node(value)
+		leader = self.traverse(index-1)
+		newNode.next = leader.next
+		leader.next = newNode
+		self.length+=1
+		return self
 
 	def traverse(self, index):
-		currentNode = self.head
+		curr = self.head
 		counter = 0
-		while(counter != index):
-			currentNode = currentNode.next
-			counter += 1
-		return currentNode
+		while(counter < index):
+			curr = curr.next
+			counter+=1
+		return curr
 
 	def remove(self, index):
-		if (self.length == 0 or index < 0 or index >= self.length):
-			return "Wrong index"
+		if (index < 0 or index >= self.length):
+			return "Nothing to remove"
 
-		if (index == 0):
+		poppped = ''
+		if (self.length == 1 and index == 0):
+			poppped = self.head
+			self.head = None
+			self.tail = None
+		elif(index == 0):
+			poppped = self.head
 			self.head = self.head.next
-		elif (index == self.length - 1):
-			leader = self.traverse(index - 1)
+		elif(index == self.length-1):
+			poppped = self.tail
+			leader = self.traverse(index-1)
 			leader.next = None
 			self.tail = leader
 		else:
-			leader = self.traverse(index - 1)
-			willBeRemoved = leader.next
-			leader.next = willBeRemoved.next
-		self.length -= 1
-		return self		
+			leader = self.traverse(index-1)
+			poppped = leader.next
+			leader.next = poppped.next
 
-myLL = LinkedList()
+		self.length-=1
+		return poppped
+
+
+	def set(self, index, value):
+		if (index < 0 or index >= self.length):
+			return "Nothing to set"
+		curr = self.traverse(index)
+		curr.value = value
+		return self
+
+	def reverse(self):
+		first = self.head
+		second = first.next
+		third = second.next
+
+		first.next = None
+		self.tail = first
+
+		while(third):
+			second.next = first
+
+			first = second
+			second = third
+			third = second.next
+
+		second.next = first
+		self.head = second
+		return self
+
+	def printValues(self):
+		array = []
+		curr = self.head
+		while(curr):
+			array.append(curr.value)
+			curr = curr.next
+		return print(array)
+
+
+myLinkedList = LinkedList()
+
+# myLinkedList.prepend(0)
+# myLinkedList.append(10)
+# myLinkedList.append(20)
+# myLinkedList.set(0,"setsetset")
+# myLinkedList.insert(0, "inserting")
+# myLinkedList.printValues()
+
+# myLinkedList.reverse()
+
+
+# myLinkedList.printValues()
+# print(myLinkedList.length)
