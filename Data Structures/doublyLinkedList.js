e38b036ac1c19165a4340beefb2af18090aa5fc2
Jongho Kim <april9288@gmail.com>
2019-01-25 12:46:29 -0800

    updating remove function for doubly linked list

--- Data Structures/doublyLinkedList.js
+++ Data Structures/doublyLinkedList.js
@@ -5,70 +5,82 @@ class Node {
 		this.prev = null
 	}
 }
+
 class DoublyLinkedList {
-	constructor(){
+	constructor (){
 		this.head = null
 		this.tail = null
-		this.length = 0
+		this.len = 0
 	}
 	append(value){
 		let newNode = new Node(value)
-		if (this.length === 0) {
+		if (this.len === 0) {
 			this.head = newNode
-			this.tail = this.head
+			this.tail = newNode
 		} else {
-			this.tail.next = newNode
 			newNode.prev = this.tail
+			this.tail.next = newNode
 			this.tail = newNode
 		}
-		this.length++
+		this.len++
 		return this
 	}
 	prepend(value){
 		let newNode = new Node(value)
-		if (this.length === 0) {
+		if (this.len === 0) {
 			this.head = newNode
 			this.tail = newNode
 		} else {
-			this.head.prev = newNode
 			newNode.next = this.head
+			this.head.prev = newNode
 			this.head = newNode
 		}
-		this.length++
+		this.len++
 		return this
 	}
-	insert(index, value){
-		if (index < 0 || index > this.length) {
-			return "Wrong index"
-		} 
-
-		if (index === 0) {
+	insert(index,value){
+		if (index < 0 || index > this.len) {
+			return "Nothing to insert"
+		}
+		if (this.len === 0 || index === 0) {
 			return this.prepend(value)
 		}
-
-		if (index === this.length) {
+		if (index === this.len) {
 			return this.append(value)
 		}
-
 		let newNode = new Node(value)
-		let leader = this.traverse(index-1)
-		let follower = leader.next
-
-		newNode.next = follower
-		newNode.prev = leader
-		follower.prev = newNode
-		leader.next = newNode
-
-		this.length++
+		let leading = this.traverse(index -1)
+		let following = leading.next
+		newNode.next = following
+		newNode.prev = leading
+		leading.next = newNode
+		following.prev = newNode
+		this.len++
+		return this
+	}
+	traverse(index){
+		let curr = this.head
+		let counter = 0
+		while (counter < index) {
+			curr = curr.next
+			counter++
+		}
+		return curr
+	}
+	set(index,value){
+		if (index < 0 || index >= this.len) {
+			return "Nothing to set"
+		}
+		let curr = this.traverse(index)
+		curr.value = value
 		return this
 	}
 	remove(index){
-		if (this.length === 0 || index < 0 || index >= this.length) {
-			return "Wrong index value"
+		if (index < 0 || index >= this.len) {
+			return "Nothing to remove"
 		}
-
 		let popped
-		if (this.length === 1) {
+		if (this.len === 1) {
 			popped = this.head
 			this.head = null
 			this.tail = null
@@ -76,42 +88,34 @@ class DoublyLinkedList {
 			popped = this.head
 			this.head = this.head.next
 			this.head.prev = null
-		} else if (index === this.length-1) {
+		} else if (index === this.len-1) {
 			popped = this.tail
-			let leader = this.traverse(index-1)
-			leader.next = null
-			this.tail = leader
-		} else {
-			let leader = this.traverse(index-1)
-			let willBeRemoved = leader.next
-			let follower = willBeRemoved.next
+			this.tail = this.tail.prev
+			this.tail.next = null
 
-			leader.next = follower
-			follower.prev = leader
+		} else {
+			let leading = this.traverse(index-1)
+			popped = leading.next
+			let following = popped.next
+			leading.next = following
+			following.prev = leading
 		}
 
-		this.length--
+		this.len--
 		return popped
 	}
-	traverse(index){
-		let curr = this.head
-		let counter = 0
-		while (counter != index) {
-			curr = curr.next
-			counter++
-		}
-		return curr
+	isEmpty(){
+		return Boolean(!this.len)
 	}
-	printArray(){
+	print(){
 		let list = []
 		let curr = this.head
-		while (curr) {
+		while(curr){
 			list.push(curr.value)
 			curr = curr.next
 		}
-		return list
+		return console.log(list)
 	}
 }
 
-
-let myDoubleLL = new DoublyLinkedList()
+let myDLL = new DoublyLinkedList()
--- Data Structures/doublyLinkedList.py
+++ Data Structures/doublyLinkedList.py
@@ -10,108 +10,107 @@ class DoublyLinkedList:
 		self.tail = None
 		self.length = 0
 
-	def printList(self):
+	def isEmpty(self):
+		return bool(self.length is 0)
+
+	def log(self):
 		array = []
 		curr = self.head
 		while (curr):
 			array.append(curr.value)
 			curr = curr.next
-		return array
+		return print(array)
 
 	def append(self, value):
 		newNode = Node(value)
 		if (self.length == 0):
 			self.head = newNode
-			self.tail = self.head
-		else:
-			self.tail.next = newNode
-			newNode.prev = self.tail
 			self.tail = newNode
+		else:
+			newNode.prev = self.tail
+			self.tail.next = newNode
+			self.tail = newNode
+
 		self.length += 1
 		return self
 
 	def prepend(self, value):
-
 		newNode = Node(value)
 		if (self.length == 0):
 			self.head = newNode
 			self.tail = newNode
 		else:
-
-			self.head.prev = newNode
 			newNode.next = self.head
+			self.head.prev = newNode
 			self.head = newNode
+
 		self.length += 1
 		return self
 
 	def insert(self, index, value):
 		if (index < 0 or index > self.length):
-			return "wrong index"
+			return "Nothing to insert"
+
+		if (self.length == 0 or index == self.length):
+			return self.append(value)
+
 		if (index == 0):
 			return self.prepend(value)
-		if (index == self.length):
-			return self.append(value)
-		
+
 		newNode = Node(value)
-		leader = self.traverse(index -1)
-		follower = leader.next
-		leader.next = newNode
-		newNode.next = follower
-		newNode.prev = leader
-		follower.prev = newNode
-		self.length += 1
+		leading = self.traverse(index-1)
+		following = leading.next
+
+		newNode.prev = leading
+		newNode.next = following
+		leading.next = newNode
+		following.prev = newNode
+		self.length+=1
 		return self
-
-	def remove(self, index):
-		if (self.length == 0 or index < 0 or index >= self.length):
-			return "Nothing to remove"
-
-		if (self.length == 1 and index == 0):
-			popped = self.head
-			self.head = None
-			self.tail = None
-			self.length -= 1
-			return popped
-
-		if (index == 0):
-			popped = self.head
-			self.head = self.head.next
-			self.head.prev = None
-			self.length -= 1
-			return popped
-
-		if (index == self.length-1):
-			popped = self.tail
-			leader = self.traverse(index -1)
-			leader.next = None
-			self.tail = leader
-			self.length -= 1
-			return popped
-
-		leader = self.traverse(index - 1)
-		popped = leader.next
-		follower = popped.next
-
-		leader.next = follower
-		follower.prev = leader
-
-		self.length -= 1
-		return popped
 
 	def traverse(self, index):
 		curr = self.head
 		counter = 0
-		while (counter != index):
+		while(counter < index):
 			curr = curr.next
 			counter+=1
 		return curr
 
-myNewDoublyLL = DoublyLinkedList()
-myNewDoublyLL.append(0)
-myNewDoublyLL.append(10)
-myNewDoublyLL.append(20)
-myNewDoublyLL.append(30)
-myNewDoublyLL.append(40)
-myNewDoublyLL.remove(5)
-print(myNewDoublyLL.length)
-print(myNewDoublyLL.printList())
+	def set(self, index, value):
+		if (index < 0 or index >= self.length):
+			return "Nothing to set"
+
+		curr = self.traverse(index)
+		curr.value = value
+		return self
+
+	def remove(self, index):
+		if (index < 0 or index >= self.length):
+			return "Nothing to remove"
+
+		popped = ''
+		if (self.length==1):
+			popped = self.head
+			self.head = None
+			self.tail = None
+		elif (index == 0):
+			popped = self.head
+			self.head = self.head.next
+			self.head.prev = None
+		elif (index == self.length-1):
+			popped = self.tail
+			self.tail = self.tail.prev
+			self.tail.next = None
+		else:
+			leading = self.traverse(index-1)
+			popped = leading.next
+			following = popped.next
+
+			leading.next = following
+			following.prev = leading
+
+
+		self.length-=1
+		return popped
+
+myNewDLL = DoublyLinkedList()
